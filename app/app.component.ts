76cb7e13f2048bbd3a82e4211dc3c8717a40c148
Ben Vissotzky <ben.vissotzky@gmail.com>
2016-10-21 08:28:58 -0700

    initial commit, directories established, app.component, app.module created

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <h4>Meal Tracker to the max, ya know</h4>
+  
+  `
+})
+
+export class AppComponent {
+}
